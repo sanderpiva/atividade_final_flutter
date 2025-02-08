a8a91f6302782cc0e2cfe37c05cf3a87922cbbb7
sanderpiva <84524010+sanderpiva@users.noreply.github.com>
2025-02-08 18:42:41 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Comparador de Combustíveis',
+      title: 'Radar de Combustível',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
